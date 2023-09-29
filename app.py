7372611c82f43978a5e9658e2fcba27b612afe61
paresh95 <paresh.sharma@itv.com>
2023-09-29 14:52:56 +0100

    PS|WIP-facial texture

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import gradio as gr
+from cv_utils.facial_texture import compute_face_simplicity
 
 def identity_function(input_image):
     return input_image
 
 iface = gr.Interface(
-    fn=identity_function,
+    fn=compute_face_simplicity,
     inputs=gr.inputs.Image(type="pil"),
     outputs=gr.outputs.Image(type="pil")
 )
