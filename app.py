1ed5ce56b79494cde76679b747eb0b3a471fbf2e
paresh95 <paresh.sharma@itv.com>
2023-09-29 11:04:46 +0100

    PS|Add image interface

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+def identity_function(input_image):
+    return input_image
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+iface = gr.Interface(
+    fn=identity_function,
+    inputs=gr.inputs.Image(),
+    outputs=gr.outputs.Image()
+)
+
 iface.launch(share=True)
