4915b467e9197273775d9738399f1a940e9ca4db
paresh95 <paresh.sharma@itv.com>
2023-09-30 16:04:24 +0100

    PS|Updated app to handle two functions

--- app.py
+++ app.py
@@ -2,16 +2,24 @@ import gradio as gr
 from utils.face_texture import GetFaceTexture
 from utils.face_symmetry import GetFaceSymmetry
 
-iface = gr.Interface(
-    fn=GetFaceTexture().main,
-    inputs=gr.inputs.Image(type="pil"),
-    outputs=[gr.outputs.Image(type="pil"), gr.outputs.Image(type="pil"), "text"],
-)
+
+def combined_fn(input_image):
+    texture_results = GetFaceTexture().main(input_image)
+    symmetry_results = GetFaceSymmetry().main(input_image)
+    return (*texture_results, *symmetry_results)
+
 
 iface = gr.Interface(
-    fn=GetFaceSymmetry().main,
+    fn=combined_fn,
     inputs=gr.inputs.Image(type="pil"),
-    outputs=[gr.outputs.Image(type="pil"), "text"],
+    outputs=[
+        gr.outputs.Image(type="pil"),  # From GetFaceTexture
+        gr.outputs.Image(type="pil"),  # From GetFaceTexture
+        "text",                        # From GetFaceTexture
+        gr.outputs.Image(type="pil"),  # From GetFaceSymmetry
+        "text"                         # From GetFaceSymmetry
+    ]
 )
 
 iface.launch()
+
